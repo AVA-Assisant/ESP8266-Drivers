2e47b2e05d6794df8572a45621b9573647aea1f3
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-01-08 00:56:37 +0100

    Publisher of led state

---
+++ Led_on:off/publish.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import json
+
+
+def on_connect(mqttc, obj, flags, rc):
+    print("Connected!")
+
+
+mqttc = mqtt.Client("Publisher")
+mqttc.on_connect = on_connect
+
+mqttc.connect("192.168.1.191", 2000)
+
+mqttc.loop_start()
+
+while True:
+    q = input("Led: ")
+    if q == "on":
+        mqttc.publish("led", json.dumps({"state": True}))
+    elif q == "off":
+        mqttc.publish("led", json.dumps({"state": False}))
