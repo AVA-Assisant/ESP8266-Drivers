66a0ba7086ffd13b2fac95f5c90f5ab2903195d6
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-01-07 19:36:38 +0100

    Publish.py console messages

--- Led_on:off/publish.py
+++ Led_on:off/publish.py
@@ -1,34 +1,13 @@
-
-
 import paho.mqtt.client as mqtt
 import json
 
 
-# def on_connect(mqttc, obj, flags, rc):
-#     print("rc: " + str(rc))
-
-
-# def on_message(mqttc, obj, msg):
-#     print(msg.topic + " " + str(msg.payload))
-
-
-# def on_publish(mqttc, obj, mid):
-#     print("mid: " + str(mid))
-
-
-# def on_subscribe(mqttc, obj, mid, granted_qos):
-#     print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-
-# def on_log(mqttc, obj, level, string):
-#     print(string)
+def on_connect(mqttc, obj, flags, rc):
+    print("Connected!")
 
 
 mqttc = mqtt.Client("Publisher")
-# mqttc.on_message = on_message
-# mqttc.on_connect = on_connect
-# mqttc.on_publish = on_publish
-# mqttc.on_subscribe = on_subscribe
+mqttc.on_connect = on_connect
 
 mqttc.connect("192.168.1.191", 2000)
 
